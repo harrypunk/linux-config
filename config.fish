2b9be3a06f28fa67c2fc6a7c3dda2be9fe11ea2f
harrypunk <allthatjazz@outlook.com>
2025-03-22 18:53:34 +0800

    Add rustup config

--- config.fish
+++ config.fish
@@ -1,3 +1,6 @@
-set -x PATH ~/.local/bin ~/go/bin $PATH
+set -x RUSTUP_DIST_SERVER "https://rsproxy.cn"
+set -x RUSTUP_UPDATE_ROOT "https://rsproxy.cn/rustup"
+
+set -x PATH ~/.local/bin ~/go/bin ~/.cargo/bin $PATH
 
 zoxide init fish | source
